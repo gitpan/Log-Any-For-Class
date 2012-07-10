8918efe9771c93547030971cd7d840cb09dadb87
Steven Haryanto <stevenharyanto@gmail.com>
2012-07-10 16:19:46 +0000

    Import of SHARYANTO/Log-Any-For-Class-0.05 from CPAN.
    gitpan-cpan-distribution: Log-Any-For-Class
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         SHARYANTO/Log-Any-For-Class-0.05.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Log/Any/For/Class.pm
@@ -0,0 +1,204 @@
+package Log::Any::For::Class;
+
+use 5.010;
+use strict;
+use warnings;
+use Log::Any '$log';
+
+our $VERSION = '0.05'; # VERSION
+
+use Data::Clone;
+use Scalar::Util qw(blessed);
+# doesn't currently work, Log::Log4perl not fooled
+#use Sub::Uplevel;
+
+our %SPEC;
+require Exporter;
+use Log::Any::For::Package qw(add_logging_to_package);
+our @ISA = qw(Log::Any::For::Package Exporter);
+our @EXPORT_OK = qw(add_logging_to_class);
+
+sub _default_precall_logger {
+    my $args  = shift;
+    my $margs = $args->{args};
+
+    #uplevel 2, $args->{orig}, @$margs;
+
+    # exclude $self or package
+    $margs->[0] = '$self' if blessed($margs->[0]);
+
+    $log->tracef("---> %s(%s)", $args->{name}, $margs);
+}
+
+sub _default_postcall_logger {
+    my $args = shift;
+    #uplevel 2, $args->{orig}, @{$args->{args}};
+
+    if (@{$args->{result}}) {
+        $log->tracef("<--- %s() = %s", $args->{name}, $args->{result});
+    } else {
+        $log->tracef("<--- %s()", $args->{name});
+    }
+}
+
+my $spec = $Log::Any::For::Package::SPEC{add_logging_to_package};
+$spec->{summary} = 'Add logging to class';
+$spec->{description} = <<'_';
+
+Logging will be done using Log::Any.
+
+Currently this function adds logging around method calls, e.g.:
+
+    -> Class::method(...)
+    <- Class::method() = RESULT
+    ...
+
+_
+delete $spec->{args}{packages};
+$spec->{args}{classes} = {
+    summary => 'Classes to add logging to',
+    schema => ['array*' => {of=>'str*'}],
+    req => 1,
+    pos => 0,
+};
+delete $spec->{args}{filter_subs};
+$spec->{args}{filter_methods} = {
+    summary => 'Filter methods to add logging to',
+    schema => ['array*' => {of=>'str*'}],
+    description => <<'_',
+
+The default is to add logging to all non-private methods. Private methods are
+those prefixed by `_`.
+
+_
+};
+$SPEC{add_logging_to_class} = $spec;
+sub add_logging_to_class {
+    my %args = @_;
+
+    my $classes = $args{classes} or die "Please specify 'classes'";
+    $classes = [$classes] unless ref($classes) eq 'ARRAY';
+    delete $args{classes};
+
+    my $filter_methods = $args{filter_methods};
+    delete $args{filter_methods};
+
+    $args{precall_logger}  //= \&_default_precall_logger;
+    $args{postcall_logger} //= \&_default_postcall_logger;
+
+    add_logging_to_package(
+        %args,
+        packages => $classes,
+        filter_subs => $filter_methods,
+    );
+}
+
+1;
+# ABSTRACT: Add logging to class
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Log::Any::For::Class - Add logging to class
+
+=head1 VERSION
+
+version 0.05
+
+=head1 SYNOPSIS
+
+ use Log::Any::For::Class qw(add_logging_to_class);
+ add_logging_to_class(classes => [qw/My::Class My::SubClass/]);
+ # now method calls to your classes are logged, by default at level 'trace'
+
+=head1 CREDITS
+
+Some code portion taken from L<Devel::TraceMethods>.
+
+=head1 SEE ALSO
+
+L<Log::Any::For::Package>
+
+L<Log::Any::For::DBI>, an application of this module.
+
+=head1 DESCRIPTION
+
+
+This module has L<Rinci> metadata.
+
+=head1 FUNCTIONS
+
+
+None are exported by default, but they are exportable.
+
+=head2 add_logging_to_class(%args) -> [status, msg, result, meta]
+
+Add logging to class.
+
+Logging will be done using Log::Any.
+
+Currently this function adds logging around method calls, e.g.:
+
+    -> Class::method(...)
+    <- Class::method() = RESULT
+    ...
+
+Arguments ('*' denotes required arguments):
+
+=over 4
+
+=item * B<classes>* => I<array>
+
+Classes to add logging to.
+
+=item * B<filter_methods>* => I<array>
+
+Filter methods to add logging to.
+
+The default is to add logging to all non-private methods. Private methods are
+those prefixed by C<_>.
+
+=item * B<postcall_logger>* => I<code>
+
+Supply custom postcall logger.
+
+Just like precallI<logger, but code will be called after method is call. Code
+will be given a hash argument %args containing these keys: C<args> (arrayref, the
+original @>), C<orig> (coderef, the original method), C<name> (string, the
+fully-qualified method name), C<result> (arrayref, the method result).
+
+You can use this mechanism to customize logging.
+
+=item * B<precall_logger>* => I<code>
+
+Supply custom precall logger.
+
+Code will be called when logging method call. Code will be given a hash argument
+%args containing these keys: C<args> (arrayref, the original @_), C<orig>
+(coderef, the original method), C<name> (string, the fully-qualified method
+name).
+
+You can use this mechanism to customize logging.
+
+=back
+
+Return value:
+
+Returns an enveloped result (an array). First element (status) is an integer containing HTTP status code (200 means OK, 4xx caller error, 5xx function error). Second element (msg) is a string containing error message, or 'OK' if status is 200. Third element (result) is optional, the actual result. Fourth element (meta) is called result metadata and is optional, a hash that contains extra information.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
---
+++ lib/Log/Any/For/Package.pm
@@ -0,0 +1,315 @@
+package Log::Any::For::Package;
+
+use 5.010;
+use strict;
+use warnings;
+use Log::Any '$log';
+
+our $VERSION = '0.05'; # VERSION
+
+#use Sub::Uplevel;
+
+our %SPEC;
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(add_logging_to_package);
+
+# XXX copied from SHARYANTO::Package::Util
+sub package_exists {
+    no strict 'refs';
+
+    my $pkg = shift;
+
+    return unless $pkg =~ /\A\w+(::\w+)*\z/;
+    if ($pkg =~ s/::(\w+)\z//) {
+        return !!${$pkg . "::"}{$1 . "::"};
+    } else {
+        return !!$::{$pkg . "::"};
+    }
+}
+
+sub _default_precall_logger {
+    my $args = shift;
+    #uplevel 2, $args->{orig}, @{$args->{args}};
+
+    $log->tracef("---> %s(%s)", $args->{name}, $args->{args});
+}
+
+sub _default_postcall_logger {
+    my $args = shift;
+    #uplevel 2, $args->{orig}, @{$args->{args}};
+
+    if (@{$args->{result}}) {
+        $log->tracef("<--- %s() = %s", $args->{name}, $args->{result});
+    } else {
+        $log->tracef("<--- %s()", $args->{name});
+    }
+}
+
+$SPEC{add_logging_to_package} = {
+    v => 1.1,
+    summary => 'Add logging to package',
+    description => <<'_',
+
+Logging will be done using Log::Any.
+
+Currently this function adds logging around function calls, e.g.:
+
+    -> Package::func(...)
+    <- Package::func() = RESULT
+    ...
+
+_
+    args => {
+        packages => {
+            summary => 'Packages to add logging to',
+            schema => ['array*' => {of=>'str*'}],
+            req => 1,
+            pos => 0,
+        },
+        precall_logger => {
+            summary => 'Supply custom precall logger',
+            schema  => 'code*',
+            description => <<'_',
+
+Code will be called when logging method call. Code will be given a hash argument
+%args containing these keys: `args` (arrayref, the original @_), `orig`
+(coderef, the original method), `name` (string, the fully-qualified method
+name).
+
+You can use this mechanism to customize logging.
+
+_
+        },
+        postcall_logger => {
+            summary => 'Supply custom postcall logger',
+            schema  => 'code*',
+            description => <<'_',
+
+Just like precall_logger, but code will be called after method is call. Code
+will be given a hash argument %args containing these keys: `args` (arrayref, the
+original @_), `orig` (coderef, the original method), `name` (string, the
+fully-qualified method name), `result` (arrayref, the method result).
+
+You can use this mechanism to customize logging.
+
+_
+        },
+        filter_subs => {
+            summary => 'Filter subroutines to add logging to',
+            schema => ['any*' => {of=>['regex*', 'code*']}],
+            description => <<'_',
+
+The default is to add logging to all non-private subroutines. Private
+subroutines are those prefixed by `_`.
+
+_
+        },
+    },
+    result_naked => 1,
+};
+sub add_logging_to_package {
+
+    my %args = @_;
+
+    my $packages = $args{packages} or die "Please specify 'packages'";
+    $packages = [$packages] unless ref($packages) eq 'ARRAY';
+
+    my $filter = $args{filter_subs} // qr/[^_]/;
+
+    for my $package (@$packages) {
+
+        die "Invalid package name $package"
+            unless $package =~ /\A\w+(::\w+)*\z/;
+
+        # require module
+        unless (package_exists($package)) {
+            eval "use $package; 1" or die "Can't load $package: $@";
+        }
+
+        my $src;
+        # get the calling package symbol table name
+        {
+            no strict 'refs';
+            $src = \%{ $package . '::' };
+        }
+
+        # loop through all symbols in calling package, looking for subs
+        for my $symbol (keys %$src) {
+            # get all code references, make sure they're valid
+            my $sub = *{ $src->{$symbol} }{CODE};
+            next unless defined $sub and defined &$sub;
+
+            my $name = "${package}::$symbol";
+            if (ref($filter) eq 'CODE') {
+                next unless $filter->($name);
+            } else {
+                next unless $name =~ $filter;
+            }
+
+            # save all other slots of the typeglob
+            my @slots;
+
+            for my $slot (qw( SCALAR ARRAY HASH IO FORMAT )) {
+                my $elem = *{ $src->{$symbol} }{$slot};
+                next unless defined $elem;
+                push @slots, $elem;
+            }
+
+            # clear out the source glob
+            undef $src->{$symbol};
+
+            # replace the sub in the source
+            $src->{$symbol} = sub {
+                my $logger;
+                my @args = @_;
+                my %largs = (
+                    orig   => $sub,
+                    name   => $name,
+                    args   => [@args],
+                );
+
+                $logger = $args{precall_logger} // \&_default_precall_logger;
+                $logger->(\%largs);
+
+                my $wa = wantarray;
+                my @res;
+                if ($wa) {
+                    @res =  $sub->(@args);
+                } elsif (defined $wa) {
+                    $res[0] = $sub->(@args);
+                } else {
+                    $sub->(@args);
+                }
+
+                $logger = $args{postcall_logger} // \&_default_postcall_logger;
+                $largs{result} = \@res;
+                $logger->(\%largs);
+
+                if ($wa) {
+                    return @res;
+                } elsif (defined $wa) {
+                    return $res[0];
+                } else {
+                    return;
+                }
+            };
+
+            # replace the other slot elements
+            for my $elem (@slots) {
+                $src->{$symbol} = $elem;
+            }
+        } # for $symbol
+
+    } # for $package
+
+    1;
+}
+
+1;
+# ABSTRACT: Add logging to package
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Log::Any::For::Package - Add logging to package
+
+=head1 VERSION
+
+version 0.05
+
+=head1 SYNOPSIS
+
+ use Log::Any::For::Package qw(add_logging_to_package);
+ add_logging_to_package(packages => [qw/My::Module My::Other::Module/]);
+ # now calls to your module functions are logged, by default at level 'trace'
+
+=head1 CREDITS
+
+Some code portion taken from L<Devel::TraceMethods>.
+
+=head1 SEE ALSO
+
+L<Log::Any::For::Class>
+
+=head1 DESCRIPTION
+
+
+This module has L<Rinci> metadata.
+
+=head1 FUNCTIONS
+
+
+None are exported by default, but they are exportable.
+
+=head2 add_logging_to_package(%args) -> [status, msg, result, meta]
+
+Add logging to class.
+
+Logging will be done using Log::Any.
+
+Currently this function adds logging around method calls, e.g.:
+
+    -> Class::method(...)
+    <- Class::method() = RESULT
+    ...
+
+Arguments ('*' denotes required arguments):
+
+=over 4
+
+=item * B<classes>* => I<array>
+
+Classes to add logging to.
+
+=item * B<filter_methods>* => I<array>
+
+Filter methods to add logging to.
+
+The default is to add logging to all non-private methods. Private methods are
+those prefixed by C<_>.
+
+=item * B<postcall_logger>* => I<code>
+
+Supply custom postcall logger.
+
+Just like precallI<logger, but code will be called after method is call. Code
+will be given a hash argument %args containing these keys: C<args> (arrayref, the
+original @>), C<orig> (coderef, the original method), C<name> (string, the
+fully-qualified method name), C<result> (arrayref, the method result).
+
+You can use this mechanism to customize logging.
+
+=item * B<precall_logger>* => I<code>
+
+Supply custom precall logger.
+
+Code will be called when logging method call. Code will be given a hash argument
+%args containing these keys: C<args> (arrayref, the original @_), C<orig>
+(coderef, the original method), C<name> (string, the fully-qualified method
+name).
+
+You can use this mechanism to customize logging.
+
+=back
+
+Return value:
+
+Returns an enveloped result (an array). First element (status) is an integer containing HTTP status code (200 means OK, 4xx caller error, 5xx function error). Second element (msg) is a string containing error message, or 'OK' if status is 200. Third element (result) is optional, the actual result. Fourth element (meta) is called result metadata and is optional, a hash that contains extra information.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
